15e810cf6ca3b4167e532cc8ba64a08d7e97800b
Pedro Araujo <ppteixeira@outlook.com>
2017-03-01 08:33:21 +1100

    Fix Gigasecond

--- gigasecond/gigasecond.rb
+++ gigasecond/gigasecond.rb
@@ -3,8 +3,7 @@ module BookKeeping
 end
 
 class Gigasecond
-  def self.from(from_time)
-    to_time = from_time + 1_000_000_000 # 10**9
-    return to_time
+  def self.from(given_time)
+    to_time = given_time + 10**9 #1_000_000_000 
   end
 end
